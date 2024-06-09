815d310659db62fb78d484860d7ff9cef14a2866
nchlnnmrvnz <114238074+nchlnnmrvnz@users.noreply.github.com>
2024-06-09 15:12:41 +0800

    fix: DELETE firebase-config

--- abc-store/app/firebase-config.js
+++ abc-store/app/firebase-config.js
@@ -2,13 +2,6 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-     apiKey: "AIzaSyCHN4Zjw5072TSLpnpmXF_RNPjVaniKIR8",
-     authDomain: "abc-store-123.firebaseapp.com",
-     projectId: "abc-store-123",
-     storageBucket: "abc-store-123.appspot.com",
-     messagingSenderId: "285455922954",
-     appId: "1:285455922954:web:5a9b36cef2d20b6c1b2cf3",
-     measurementId: "G-865RQL9YC7"
 };
 
 const app = initializeApp(firebaseConfig);
